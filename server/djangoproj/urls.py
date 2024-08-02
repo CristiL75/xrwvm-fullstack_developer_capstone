3d963539907c38eb03647238f7ae2c1e010da8a1
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2024-08-02 19:36:53 +0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -8,7 +8,6 @@ app_name = 'djangoapp'
 urlpatterns = [
     # Path for logout
     path('logout/', views.logout_request, name='logout'),
-    
     # Path for registration
     path('register/', views.registration, name='register'),
 
@@ -18,29 +17,29 @@ urlpatterns = [
     # Path for get_dealerships
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path(
-        'get_dealers/<str:state>/', 
-        views.get_dealerships, 
+        'get_dealers/<str:state>/',
+        views.get_dealerships,
         name='get_dealers_by_state'
     ),
 
     # Path for dealer details
     path(
         'dealer/<int:dealer_id>/', 
-        views.get_dealer_details, 
+        views.get_dealer_details,
         name='dealer_details'
     ),
 
     # Path for adding a review
     path(
-        'add_review/', 
-        views.add_review, 
+        'add_review/',
+        views.add_review,
         name='add_review'
     ),
 
     # Path for reviews from dealer_id
     path(
-        'reviews/dealer/<int:dealer_id>/', 
-        views.get_dealer_reviews, 
+        'reviews/dealer/<int:dealer_id>/',
+        views.get_dealer_reviews,
         name='dealer_reviews'
     ),
 
