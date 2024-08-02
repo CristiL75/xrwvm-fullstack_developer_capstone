573cf3332c6f98dad334d7615bda8c0ac8f1a8c0
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2024-08-02 19:27:39 +0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,13 +19,13 @@ urlpatterns = [
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
     path(
         'get_dealers/<str:state>/',
-         views.get_dealerships, 
+        views.get_dealerships, 
         name='get_dealers_by_state'
     ),
 
     # Path for dealer details
     path('dealer/<int:dealer_id>/',
-         views.get_dealer_details, 
+         views.get_dealer_details,
          name='dealer_details'
         ),
 
