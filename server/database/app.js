11a48811535541094e836758f908cbf34d8c3fec
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2024-08-02 17:06:14 +0300

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -22,9 +22,9 @@ const Dealerships = require('./dealership');
 (async () => {
   try {
     await Reviews.deleteMany({});
-    await Reviews.insertMany(reviews_data.reviews); // Dot notation used
+    await Reviews.insertMany(reviews_data.reviews);
     await Dealerships.deleteMany({});
-    await Dealerships.insertMany(dealerships_data.dealerships); // Dot notation used
+    await Dealerships.insertMany(dealerships_data.dealerships);
   } catch (error) {
     console.error('Error initializing database:', error);
   }
@@ -90,7 +90,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   try {
     const data = JSON.parse(req.body);
     const documents = await Reviews.find().sort({ id: -1 });
-    const new_id = documents[0]?.id + 1 || 1; // Dot notation used
+    const new_id = documents[0]?.id + 1 || 1;
 
     const review = new Reviews({
       id: new_id,
