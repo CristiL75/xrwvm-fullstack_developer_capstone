874737e91096245b9a297f0bba41e103f8d70767
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2024-08-02 17:58:17 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,16 +5,19 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -23,22 +26,14 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-                               validators=[
-                                   MaxValueValidator(2023),
-                                   MinValueValidator(2015)])
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
-
-
-class Car(models.Model):
-    car_model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
-    color = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    mileage = models.PositiveIntegerField()
-    # Add other fields as needed
-
-    def __str__(self):
-        return f"{self.car_model.name} ({self.color})"
-
+        return self.name  # Return the name as the string representation
